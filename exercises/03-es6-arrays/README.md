41538e2ea3fd21ba4a21a4f948ad2b63b56c59ff
David <david.vangompel@gmail.com>
2021-09-18 20:54:17 +0200

    Exercises

--- exercises/03-es6-arrays/README.md
+++ exercises/03-es6-arrays/README.md
@@ -1,1 +1,10 @@
 # ES6 - Declarative programming with array methods
+
+1. Clone or download this repository
+2. run `npm install` in the exercises/03-es6 directory
+3. run `npm start`
+4. Open browser at http://localhost:1234 and open devtools
+5. Read todos in src/declarative.js
+6. See the output in the browser devtools
+7. When finished, edit src/index.html to load array_methods.js instead of declarative.js
+8. Add your solution as a second argument of the console.logs
---
+++ exercises/03-es6-arrays/src/array_methods.js
@@ -0,0 +1,160 @@
+// Use the array method most fit for the exercise.
+// Put the answer as the second argument to the console method
+
+console.log("-- Exercise 1 --");
+{
+  const array = ["a", "b", "c"];
+
+  console.log('\tElement "b" can be found at index: ' /*, your solution */);
+}
+
+console.log("-- Exercise 2 --");
+{
+  const array = [0, "", false];
+
+  console.log('\tDoes "array" contain truthy values?');
+}
+
+console.log("-- Exercise 3 --");
+{
+  const array = [0, "", -1, false];
+
+  console.log('\tDoes "array" contain truthy values?');
+}
+
+console.log("-- Exercise 4 --");
+{
+  const array = ["apple", "banana", "cherry"];
+
+  console.log('\tPrint "array" backwards: ');
+}
+
+console.log("-- Exercise 5 --");
+{
+  const array = [["apple", "banana"], ["tomato", "cucumber"]];
+
+  console.log('\tPrint values from "array", without nesting: ');
+}
+
+console.log("-- Exercise 6 --");
+{
+  const array = [["apple", "banana"], [["tomato", "cucumber"]]];
+
+  console.log('\tPrint values from "array", without nesting: ');
+}
+
+console.log("-- Exercise 7 --");
+{
+  const array = ["apple", "banana", "kiwi"];
+
+  console.log('\tPrint "array" as a string, concatenated by a comma: ');
+}
+
+console.log("-- Exercise 8 --");
+{
+  const array = ["apple", "banana", "cherry", "kiwi"];
+
+  console.log('\tPrint "array" as a string, concatenated by a dash(-): ');
+}
+
+console.log("-- Exercise 9 --");
+{
+  const array = [
+    "Light Amplification",
+    "by the Stimulated Emission",
+    "of Radiation"
+  ];
+
+  console.log(
+    '\tMake a new array from "array", but with all words as separate items: '
+  );
+}
+
+console.log("-- Exercise 10 --");
+{
+  const array = [10, 8, 30, 6, 7, 100, 45];
+
+  console.log('\tAre all values in "array" bigger than 5?');
+}
+
+console.log("-- Exercise 11 --");
+{
+  const array = ["apple", "banana", "cherry", "kiwi"];
+
+  console.log('\tDoes the array contains the fruit "cherry"?');
+}
+
+console.log("-- Exercise 12 --");
+{
+  const array = ["apple", "banana", "cherry", "kiwi"];
+
+  console.log(
+    '\tDoes the array contains the fruit "banana", after the third element ("cherry")?'
+  );
+}
+
+console.log("-- Exercise 13 --");
+{
+  const array = ["apple", "banana", "cherry", "kiwi"];
+
+  console.log('\tPrint the first 2 items in "array":');
+}
+
+console.log("-- Exercise 14 --");
+{
+  const array = ["apple", "banana", "cherry", "kiwi"];
+
+  console.log('\tGive me all but the last element from "array":');
+}
+
+console.log("-- Exercise 15 --");
+{
+  const array = ["kiwi", "banana", "cherry", "apple"];
+
+  console.log('\tSort "array" alphabetically:');
+}
+
+console.log("-- Exercise 16 --");
+{
+  const array = ["kiwi", "banana", "cherry", "apple"];
+
+  console.log('\tWhat is the last item in "array"?');
+}
+
+console.log("-- Exercise 17 --");
+{
+  const array = ["Jan", "February", "April", "May"];
+
+  console.log('\tInsert "March" at the correct index in "array"');
+}
+
+console.log("-- Exercise X --");
+
+{
+  const array = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+
+  console.log("\tWhat is the sum of all elements in array?");
+}
+
+console.log("-- Exercise Y --");
+
+{
+  const array = ["Self", "Contained", "Underwater", "Breathing", "Apparatus"];
+
+  console.log(
+    '\tFind the acronym: Make a word with the first letter of every word in "array":'
+  );
+}
+
+console.log("-- Exercise Z --");
+{
+  const array = [
+    "Light Amplification",
+    "by the Stimulated Emission",
+    "of Radiation"
+  ];
+
+  console.log(
+    "\tFind the acronym: Take the first letter of every capitalized word and concatenate:"
+  );
+}
